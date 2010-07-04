e2b90bbd094dda3b4663dd42d0277a78a1c57bae
Sam Newman <sam.newman@gmail.com>
2010-07-04 19:58:12 +0100

    Merged the last of the seperate test files in.

--- src/com/thoughtworks/codelapse/git.clj
+++ src/com/thoughtworks/codelapse/git.clj
@@ -1,5 +1,5 @@
 (ns com.thoughtworks.codelapse.git
-  (:use com.thoughtworks.codelapse.utils))
+  (:use com.thoughtworks.codelapse.utils midje.semi-sweet clojure.test))
 
 (defn current-head
   [git-executor]
@@ -22,3 +22,23 @@
   user=> (git 'log')"
   [git-executable git-directory working-directory]
   (partial execute git-executable (str "--git-dir=" git-directory) (str "--work-tree=" working-directory)))
+
+(deftest should-get-current-head
+  (let [mock-executor (fn [args] (is (= args "log --format=format:'%H' -1")) "Log Output")]
+    (is (=
+      (current-head mock-executor)
+      "Log Output"))))
+
+(deftest should-list-all-commits
+  (let [mock-executor (fn [args] (is (= args "--no-pager log --format=format:'%H\t%ai' --date=iso")) "Commit1\tDate1\nCommit2\tDate2")]
+    (is (=
+      (list-all-commits mock-executor)
+      '(("Commit1" "Date1")("Commit2" "Date2"))))))
+
+(deftest should-execute-hard-reset
+  (let [mock-executor (fn [args] (is (= args "reset --hard somehash")))]
+    (is (=
+      (hard-reset mock-executor "somehash")
+      true))))
+
+(run-tests 'com.thoughtworks.codelapse.git)
