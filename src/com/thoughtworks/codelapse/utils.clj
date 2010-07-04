50efbdc318402c0e93f4acc6522a9f6e8458a2a5
Sam Newman <sam.newman@gmail.com>
2010-07-04 14:23:30 +0100

    Playing around with Midje. Added cloc

--- src/com/thoughtworks/codelapse/utils.clj
+++ src/com/thoughtworks/codelapse/utils.clj
@@ -3,7 +3,7 @@
   (:import
     (java.io IOException ByteArrayOutputStream)
     (java.lang RuntimeException)
-    (org.apache.commons.exec CommandLine DefaultExecutor ExecuteWatchdog PumpStreamHandler)))
+    (org.apache.commons.exec CommandLine DefaultExecutor ExecuteWatchdog PumpStreamHandler ExecuteException)))
 
 (defn split [str delimiter]
   (seq (.split str delimiter)))
@@ -19,6 +19,7 @@
   "Executes a command-line program, returning stdout if a zero return code, else the
   error out. Takes a list of strings which represent the command & arguments"
   [& args]
+
   (let [output-stream (new ByteArrayOutputStream)
         error-stream (new ByteArrayOutputStream)
         stream-handler (new PumpStreamHandler output-stream error-stream)
@@ -27,7 +28,10 @@
                   (.setExitValue 0)
                   (.setStreamHandler stream-handler)
                   (.setWatchdog (new ExecuteWatchdog 20000)))]
+
+    (try
      (if (= 0 (.execute executor (CommandLine/parse (apply str (interpose " " args)))))
        (.toString output-stream)
-       (.toString error-stream))))
+       (.toString error-stream))
+      (catch Exception e (throw (new RuntimeException (str "Error: " (.getMessage e) " Output:" (.toString error-stream)) e))))))
 
