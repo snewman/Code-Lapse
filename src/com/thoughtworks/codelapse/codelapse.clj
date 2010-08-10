d7f13513896244be85215d887af86866fd3cf204
Sam Newman <sam.newman@gmail.com>
2010-08-10 22:05:30 +0100

    Hacking on some code to get table manipulation working. Need to pull it out, reimplement it with TDD. But the basics are there to start allowing graphing of information with Incanter.

--- src/com/thoughtworks/codelapse/codelapse.clj
+++ src/com/thoughtworks/codelapse/codelapse.clj
@@ -2,18 +2,49 @@
   (:use com.thoughtworks.codelapse.git com.thoughtworks.codelapse.utils com.thoughtworks.codelapse.cloc))
 
 (defn cloc-for-head
-  [git-repo working-dir hash]
+  [git-repo working-dir commit]
+  "Commit is a triple containing a hash and date"
   (do
-    (hard-reset git-repo hash)
+    (print (str "processing " commit))
+    (hard-reset git-repo (first commit))
     (cloc working-dir)))
 
 (defn end-to-end-git []
   "Checks out the given repo to a temp directory, and cycles throgh all commits"
   (let [tmp-dir (make-temp-dir)
         git-exeuctable (git-executable "/usr/local/bin/git" tmp-dir tmp-dir)
-        git-repo (clone "/usr/local/bin/git" "git@github.com:snewman/Code-Lapse.git" tmp-dir)
-        ]
-    (print (map (partial cloc-for-head git-repo tmp-dir) (map first (list-all-commits git-repo))))))
+        git-repo (clone "/usr/local/bin/git" "git@github.com:snewman/Code-Lapse.git" tmp-dir)]
+    (print (map (partial cloc-for-head git-repo tmp-dir) (list-all-commits git-repo)))))
+
+(defn as-columns
+  [records]
+  (apply (partial conj #{}) (mapcat keys (map second records))))
+
+(defn header-row
+  [records]
+  (str "Date," (apply str (interpose "," (as-columns records)))))
+
+(defn values-for-record
+  [columns values]
+  (for [col columns] (get values col "-")))
+
+(defn as-row
+  [record columns]
+  (let [day (first record)
+        values (second record)]
+   (str day ","
+    (apply str (interpose "," (values-for-record columns values))))))
+
+(defn as-data
+  [records]
+  (apply str
+    (interpose "\n"
+      (for [record records]
+          (as-row record (as-columns records))))))
+
+(defn as-table
+  [records]
+    (apply str (header-row records) "\n" (as-data records)))
 
 (end-to-end-git)
 
