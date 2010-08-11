e8fe57a3c72cf54130d95297a59053ab57ce6824
Sam Newman <sam.newman@gmail.com>
2010-08-11 14:27:07 +0100

    Further simplified code using distinct & use of map to pull keys from the map

--- src/com/thoughtworks/codelapse/codelapse.clj
+++ src/com/thoughtworks/codelapse/codelapse.clj
@@ -24,21 +24,17 @@
 
 (defn as-columns
   [records]
-  (apply (partial conj #{}) (mapcat keys (map second records))))
+  (distinct (mapcat keys (map second records))))
 
 (defn header-row
   [records]
   (str "Date," (str-join "," (as-columns records))))
 
-(defn values-for-record
-  [columns values]
-  (for [col columns] (get values col "-")))
-
 (defn as-row
   [record columns]
   (let [day (first record)
         values (second record)]
-   (str day "," (str-join "," (values-for-record columns values)))))
+   (str day "," (str-join "," (map values columns)))))
 
 (defn as-data
   [records]
@@ -55,8 +51,8 @@
     (apply str (header-row records) "\n" (as-data records)))
 
 (deftest should-extract-known-columns-from-records
-  (expect (as-columns '(("ID1" {"a" 1 "b" 2}))) => #{"a" "b"})
-  (expect (as-columns '(("ID1" {"a" 1 "b" 2}) ("ID2" {"b" 1 "c" 2}))) => #{"a" "b" "c"}))
+  (expect (as-columns '(("ID1" {"a" 1 "b" 2}))) => '("a" "b"))
+  (expect (as-columns '(("ID1" {"a" 1 "b" 2}) ("ID2" {"b" 1 "c" 2}))) => '("a" "b" "c")))
 
 (deftest should-create-header-row
   (expect (header-row '(("ID" {"a" 1 "b" 2}))) => "Date,a,b"))
@@ -66,8 +62,7 @@
   (expect (as-table '(("ID1" {"a" 1 "b" 2}) ("ID2" {"a" 3 "b" 5}))) => "Date,a,b\nID1,1,2\nID2,3,5"))
 
 (deftest should-create-sparse-table-from-records
-  (expect (as-table '(("ID1" {"b" 1 "c" 2}) ("ID2" {"a" 3 "b" 5}))) => "Date,a,b,c\nID1,-,1,2\nID2,3,5,-"))
+  (expect (as-table '(("ID-A" {"b" 1 "c" 2}) ("ID-B" {"a" 3 "b" 5}))) => "Date,b,c,a\nID-A,1,2,\nID-B,5,,3"))
 
 (run-tests 'com.thoughtworks.codelapse.codelapse)
-; (end-to-end-git)
 
